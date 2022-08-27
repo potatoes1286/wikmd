0d8b71678aca64edbeb8028d8722b6ac76ccd7c7
linbreux <29354411+Linbreux@users.noreply.github.com>
2022-08-27 14:16:55 +0200

    Search in the incomming file for TOC and return a table of content at that place

--- macros.py
+++ macros.py
@@ -1,24 +1,9 @@
+import copy
 from bs4 import BeautifulSoup
 
 # MACROS
 
-# check file for macro
-# replace the macro with html code block
-
-# TOC MACRO
-# macro: "[[TOC]]"
-# replace with:
-# - title
-#   - head 1
-#       - subhead
-#       - subhead
-#   - head 2
-#       - subhead
-#       - subhead
-
-string = "<html><body>[[TOC]]<h1>this is a test</h1><a>this is a sub par</a><h2>sub</h2><h2>sub</h2><h1>test</h1>"
-
-replace = "titel, test 123"
+string = "<html><body>[[TOC]]<h1 id='head1'>this is a test</h1><a>this is a sub par</a><h2 id='head2'>sub</h2><h3>testingggg</h3><h2>sub</h2><h1>test</h1></body></html>"
 
 class Macro:
     mdMacro = ""
@@ -27,15 +12,54 @@ class Macro:
         self.mdMacro = mdMacro
         self.file = file
 
+
 class TableOfContents(Macro):
+    """
+    MACRO: "TOC" or "TOC <depth>"
+    By default the TOC will be 3 levels.
+    you can specify the depth with a number
+    """
+
+    # first the header type, second the indent
+    headers = [
+        ["h1",0],
+        ["h2",20],
+        ["h3",40],
+        ["h4",60]
+    ]
+
     def process(self):
+        args = self.mdMacro.split(" ")
         soup = BeautifulSoup(self.file,"html.parser")
-        h1s = soup.findAll("h1")
-        toc = ""
-        for i in h1s:
-            toc += i.string
-        soup.find(text="[["+ self.mdMacro +"]]").replace_with(toc)
-        print(soup)
+        if len(args) <= 1:
+            # default depth is 3
+            args.append("3")
+        depthHeaders = [item [0] for item in self.headers[0:int(args[1])]]
+        listWithHeaders = soup.find_all(depthHeaders)
+        
+        
+        title = soup.new_tag("h4")
+        title.string = "Table of content"
+
+        # get all headers
+        newsoup = BeautifulSoup()
+        newsoup.append(title)
+        for i in listWithHeaders:
+            newHead = copy.copy(i)
+            margin = 0
+            for head, indent in self.headers:
+                if head == str(i.name):
+                    margin = indent
+                    break
+ 
+            newHead.name = "p"
+            newHead.string.wrap(soup.new_tag("a"))
+            newHead["id"] = ""
+            newHead.a['style'] = "margin-left: " + str(margin) + "px; font-weight: bold; color:grey; text-decoration: none;"
+            newHead.a["href"] = "#" + str(i.get('id'))
+            newsoup.append(newHead)
+        soup.find(text="[["+ args[0] +"]]").replace_with(newsoup)
+        return(soup)
 
 
 TOC = TableOfContents("TOC", string)
